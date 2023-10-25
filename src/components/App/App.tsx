3c01051804457b833aca99be30d30b56582834b1
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-10-25 19:17:04 +0200

    Add Repository Component and its styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,16 @@
 import { repositoriesStoreMock } from "../../mocks/repositoriesStoreMock";
 import Header from "../Header/Header";
+import Repository from "../Repository/Repository";
 import UserDetails from "../UserDetails/UserDetails";
 
 export default function App(): React.ReactElement {
   return (
     <div className="custom-container">
       <Header />
-      <UserDetails user={repositoriesStoreMock.user} />
+      <div className="flex">
+        <UserDetails user={repositoriesStoreMock.user} />
+        <Repository repository={repositoriesStoreMock.repositories[1]} />
+      </div>
     </div>
   );
 }
