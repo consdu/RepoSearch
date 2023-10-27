930e3033ba4691f45ef0b699edf7b5e3874159fc
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-10-27 14:07:56 +0200

    Add tests for UserSearch Component

--- src/components/UserSearch/UserSearch.tsx
+++ src/components/UserSearch/UserSearch.tsx
@@ -27,7 +27,7 @@ export default function UserSearch({
       <div className="relative flex items-center bg-white max-md:flex-1">
         <label
           htmlFor="username"
-          aria-label="search"
+          aria-label="username"
           className="absolute pl-3 text-lg"
         >
           <GoSearch className="text-gray-400" />
@@ -35,6 +35,7 @@ export default function UserSearch({
         <input
           id="username"
           type="text"
+          role="search"
           value={username}
           onChange={(event) => setUsername(event.target.value)}
           className="w-full rounded-md border border-gray-400 bg-white px-4 py-2 pl-9 outline-none outline-offset-0 transition-all focus:border-transparent focus:outline-primary-500"
@@ -44,7 +45,8 @@ export default function UserSearch({
       <button
         type="submit"
         disabled={isLoading}
-        className="rounded-md border border-gray-400 bg-white px-4 py-2 outline-none outline-offset-0 transition-all hover:border-transparent hover:outline-primary-500 focus:border-transparent focus:outline-primary-500 disabled:cursor-not-allowed disabled:outline-none hover:disabled:border-gray-400"
+        aria-label="search user"
+        className="rounded-md border border-gray-400 bg-white px-4 py-2 outline-none outline-offset-0 transition-all hover:border-transparent hover:outline-primary-500 focus:border-transparent focus:outline-primary-500 disabled:cursor-not-allowed disabled:text-gray-400 disabled:outline-none hover:disabled:border-gray-400"
       >
         {isLoading ? <GoHourglass /> : <GoArrowRight />}
       </button>
