ec02747394729ab35930c5653ba28243136efe31
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-10-29 17:30:14 +0100

    Add RepositoriesSection Component and refactor App component's logic

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,71 +1,24 @@
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 import { Toaster } from "sonner";
-import _debounce from "debounce";
 import { useAppDispatch, useAppSelector } from "../../store";
 import Header from "../Header/Header";
-import RepositoriesList from "../RepositoriesList/RepositoriesList";
-import RepositoriesSearch from "../RepositoriesSearch/RepositoriesSearch";
 import UserDetails from "../UserDetails/UserDetails";
 import useRepositories from "../../hooks/useRepositories/useRepositories";
 import {
   loadRepositoriesActionCreator,
-  setSearchTermActionCreator,
-  loadSearchedRepositoriesActionCreator,
   loadUserActionCreator,
   setTotalPagesActionCreator,
 } from "../../store/repositories/repositoriesSlice";
 import { RepositoryStructure } from "../../types";
-import Loader from "../Loader/Loader";
-import NoRepositoriesFound from "../NoRepositoriesFound/NoRepositoriesFound";
-import Pagination from "../Pagination/Pagination";
 import { setIsRepositoriesLoadingActionCreator } from "../../store/ui/uiSlice";
+import RepositoriesSection from "../RepositoriesSection/RepositoriesSection";
 
 export default function App(): React.ReactElement {
-  const { getRepositories, getRepositoriesBySearchTerm, getUser } =
-    useRepositories();
   const dispatch = useAppDispatch();
-  const {
-    user,
-    repositories,
-    repositoriesBySearchTerm,
-    searchTerm,
-    searchMethod,
-    initialGithubUsername,
-    currentPage,
-  } = useAppSelector((state) => state.repositoriesStore);
-  const { isRepositoriesLoading } = useAppSelector((state) => state.uiStore);
-
-  const hasNoMatchingRepositories = useMemo(
-    () => searchTerm?.length > 0 && repositoriesBySearchTerm?.length === 0,
-    [repositoriesBySearchTerm, searchTerm],
+  const { user, initialGithubUsername, currentPage } = useAppSelector(
+    (state) => state.repositoriesStore,
   );
-
-  const onSearchChange = useMemo(() => {
-    return _debounce(async (searchTerm: string) => {
-      dispatch(setSearchTermActionCreator(searchTerm));
-
-      if (searchTerm === "") {
-        dispatch(loadSearchedRepositoriesActionCreator([]));
-        return;
-      }
-
-      if (user) {
-        dispatch(setIsRepositoriesLoadingActionCreator(true));
-
-        const repositories = (await getRepositoriesBySearchTerm(
-          user.login,
-          searchTerm,
-          searchMethod,
-        )) as RepositoryStructure[];
-
-        dispatch(setIsRepositoriesLoadingActionCreator(false));
-
-        if (repositories) {
-          dispatch(loadSearchedRepositoriesActionCreator(repositories));
-        }
-      }
-    }, 350);
-  }, [dispatch, getRepositoriesBySearchTerm, user, searchMethod]);
+  const { getRepositories, getUser } = useRepositories();
 
   useEffect(() => {
     (async () => {
@@ -102,25 +55,7 @@ export default function App(): React.ReactElement {
       <main>
         <section className="flex flex-col md:flex-row">
           <UserDetails user={user} />
-          <div className="flex-1 pb-20 pt-10 md:pl-20 md:pt-0">
-            <RepositoriesSearch onSearchChange={onSearchChange} />
-            {isRepositoriesLoading && !repositoriesBySearchTerm?.length && (
-              <Loader />
-            )}
-            {hasNoMatchingRepositories && !isRepositoriesLoading && (
-              <NoRepositoriesFound />
-            )}
-            {!isRepositoriesLoading && (
-              <RepositoriesList
-                repositories={
-                  searchTerm && repositoriesBySearchTerm
-                    ? repositoriesBySearchTerm
-                    : repositories
-                }
-              />
-            )}
-            <Pagination isLoading={isRepositoriesLoading} />
-          </div>
+          <RepositoriesSection />
         </section>
       </main>
     </div>
